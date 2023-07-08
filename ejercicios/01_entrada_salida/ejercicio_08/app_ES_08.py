dcbe840d3f394bc61d4c3f5e00391669241884aa
paredesenzo <enzovparedes15@gmail.com>
2023-07-08 11:33:22 -0300

    ejercicio 8

--- ejercicios/01_entrada_salida/ejercicio_08/app_ES_08.py
+++ ejercicios/01_entrada_salida/ejercicio_08/app_ES_08.py
@@ -41,7 +41,15 @@ class App(customtkinter.CTk):
 
 
     def btn_mostrar_on_click(self):
-        pass
+        operador_a = self.txt_operador_a.get()
+        operador_b = self.txt_operador_b.get()
+
+        operador_a = int(operador_a)
+        operador_b = int(operador_b)
+
+        resultado = operador_a % operador_b
+
+        alert("Resultado", f"El resultado es: {resultado}")
         
         
     
