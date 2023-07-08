d435d4ca3b706f822af70a06f3888d881d3f522b
paredesenzo <enzovparedes15@gmail.com>
2023-07-08 11:25:36 -0300

    ejercicio 6

--- ejercicios/01_entrada_salida/ejercicio_06/app_ES_06.py
+++ ejercicios/01_entrada_salida/ejercicio_06/app_ES_06.py
@@ -41,7 +41,15 @@ class App(customtkinter.CTk):
 
 
     def btn_sumar_on_click(self):
-        pass
+        operador_a = self.txt_operador_a.get()
+        operador_b = self.txt_operador_b.get()
+
+        operador_a = int(operador_a)
+        operador_b = int(operador_b)
+
+        resultado = operador_a + operador_b
+
+        alert("resultado", f"El resultado de la suma es: {resultado}")
      
         
 if __name__ == "__main__":
