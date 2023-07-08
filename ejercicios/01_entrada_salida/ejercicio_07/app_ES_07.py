d1b0c3db06a4140c31e587ebea8ff9dbb3f6c136
paredesenzo <enzovparedes15@gmail.com>
2023-07-08 11:32:04 -0300

    ejercicio 7

--- ejercicios/01_entrada_salida/ejercicio_07/app_ES_07.py
+++ ejercicios/01_entrada_salida/ejercicio_07/app_ES_07.py
@@ -50,16 +50,50 @@ class App(customtkinter.CTk):
         self.btn_dividir.grid(row=5, pady=10, columnspan=2, sticky="nsew")
 
     def btn_sumar_on_click(self):
-        pass
+        operador_a = self.txt_operador_a.get()
+        operador_b = self.txt_operador_b.get()
+        #int (es un numero entero)
+        #float (para numero con decimal)
+        #Str
+        operador_a = float(operador_a)
+        operador_b = float(operador_b)
+
+        resultado = operador_a + operador_b
+        #{resultado:.0f} el ":.0f" es para eliminar las decimales
+        alert("Resultado", f"El resultado de la suma es: {resultado:.0f}")
 
     def btn_restar_on_click(self):
-        pass
+        operador_a = self.txt_operador_a.get()
+        operador_b = self.txt_operador_b.get()
+
+        oeprador_a = int(operador_a)
+        operador_b = int(operador_b)
+
+        resultado = oeprador_a - operador_b
+
+        alert("Resultado", f"Restando los nuemros {operador_a} y {operador_b} el resultado es: {resultado}")
 
     def btn_multiplicar_on_click(self):
-        pass
+        operador_a = self.txt_operador_a.get()
+        operador_b = self.txt_operador_b.get()
+
+        operador_a = int(operador_a)
+        operador_b = int(operador_b)
+
+        resultado = operador_a * operador_b
+
+        alert("Multiplicacion", f"La multiplicacion de {operador_a} y {operador_b} es: {resultado}")
 
     def btn_dividir_on_click(self):
-        pass
+        operador_a = self.txt_operador_a.get()
+        operador_b = self.txt_operador_b.get()
+
+        operador_a = int(operador_a)
+        operador_b = int(operador_b)
+
+        resultado = operador_a / operador_b
+        #En una division siempre el resultado aprece de forma decimal (solo el resultado)
+        alert("Division", f"La division de los numeros {operador_a} y {operador_b} es: {resultado}")
         
 if __name__ == "__main__":
     app = App()
